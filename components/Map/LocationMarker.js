67eafca1af227898e8a2ec02238f4f862fca87b9
Peter Anderson <pete.anderson@gmail.com>
2023-04-20 10:49:13 +0200

    --add: own location marker

---
+++ components/Map/LocationMarker.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+const { Marker, Popup, useMapEvents } = require("react-leaflet");
+
+export default function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    locationfound: (e) => {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  useEffect(() => {
+    console.log("about to locate map");
+    map.locate();
+  }, [map]);
+
+  return (
+    position && (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    )
+  );
+}
